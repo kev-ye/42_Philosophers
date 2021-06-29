855ac076dc577920078e0cca76930873e898e1f6
Kangkai YE <kaye@e1r10p16.42.fr>
2021-06-29 18:48:22 +0200

    init

--- philo/incs/philosophers.h
+++ philo/incs/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 14:20:00 by kaye              #+#    #+#             */
-/*   Updated: 2021/06/28 19:45:33 by kaye             ###   ########.fr       */
+/*   Updated: 2021/06/29 17:25:16 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,22 +29,27 @@
 # define NONE "\033[0m"
 # define CLR "\033[2J\033[H"
 
+/* ERROR MESSAGE */
+# define ERROR_MSG "usage: \033[1;31m./philo [nbr_of_philo] [time_to_die]\n\
+               [time_to_eat] [time_to_sleep]\n\
+               \033[1;35m[[nbr_of_time_each_philo_must_eat]]\n\033[0m"
+
 /* RETURN */
 # define ERROR 1
 # define SUCCESS 0
 
 /* MINI LIB */
-int	    ft_isdigit(int c);
-int     ft_strdigit(const char *s);
+int		ft_isdigit(int c);
+int		ft_strdigit(const char *s);
 size_t	ft_strlen(const char *s);
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
 char	*ft_itoa(int n);
-int	    ft_atoi(const char *str);
-int	    ft_strcmp(const char *s1, const char *s2);
-int	    ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 /* UTILS */
-int     __ret__(char *msg, int ret);
+int		__ret__(char *msg, int ret);
 
 #endif
--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 14:25:25 by kaye              #+#    #+#             */
-/*   Updated: 2021/06/28 19:47:07 by kaye             ###   ########.fr       */
+/*   Updated: 2021/06/29 17:25:41 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,16 @@ int args_check(char **av)
 int main(int ac, char **av)
 {
 	if (ac < 5 || ac > 6)
-		return (__ret__(B_RED"Nbr of args is not a valid !\n"NONE, ERROR));
+		return (__ret__(ERROR_MSG, ERROR));
 	if (ac == 5)
 	{
 		if (ERROR == args_check(av))
-			return (__ret__(B_RED"Not a valid args!\n"NONE, ERROR));
+			return (__ret__(ERROR_MSG, ERROR));
 	}
 	else
 	{
 		if (ERROR == args_check(av))
-			return (__ret__(B_RED"Not a valid args!\n"NONE, ERROR));
+			return (__ret__(ERROR_MSG, ERROR));
 	}
 	return (SUCCESS);
 }
